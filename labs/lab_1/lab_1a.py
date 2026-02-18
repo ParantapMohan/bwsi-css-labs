43b382fc691e420416683ac0bac7db776abe4d15
ParantapMohan <drsarikamohan2@gmail.com>
2026-02-17 21:08:48 -0800

    Update lab_1a.py
    Update, introduction for practice.

--- labs/lab_1/lab_1a.py
+++ labs/lab_1/lab_1a.py
@@ -11,6 +11,6 @@ def main():
     name = "Parantap Mohan" # TODO: Insert your name between the double quotes
 
     print(f"{name}, Welcome to the CSS course!")
-
+print("My name is Parantap Mohan and I am a sophomore at Foothill High School. I have a strong passion for physics, CS, and math.")
 if __name__ == "__main__":
     main()
